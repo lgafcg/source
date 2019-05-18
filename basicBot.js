99028cbb6ad81922600d6d94b75381d710faf561
lgafcg <49931100+lgafcg@users.noreply.github.com>
2019-05-18 20:01:31 +0600

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3305,7 +3305,7 @@
 
             rouletteCommand: {
                 command: 'roulette',
-                rank: 'mod',
+                rank: 'manager',
                 type: 'exact',
                 functionality: function(chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
